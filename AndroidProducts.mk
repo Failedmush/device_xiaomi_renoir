78d486b4a49306cbdb646d94cd03568eb5ae7a52
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-19 17:22:57 +0900

    Renoir: welcome @Voltageos

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,6 +3,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+COMMON_LUNCH_CHOICES := \
+    voltage_renoir-user \
+    voltage_renoir-userdebug \
+    voltage_renoir-eng
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_renoir.mk
